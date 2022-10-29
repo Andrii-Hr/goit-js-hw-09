fa2a6757e6e79748440f57f6a1a31ccaeb8488a4
Andrii-Hr <110282045+Andrii-Hr@users.noreply.github.com>
2022-10-29 15:03:23 +0000

    Deploying to gh-pages from @ Andrii-Hr/goit-js-hw-09@40b63e9282e49e82038c5b2dc4761137fd02c17b 🚀

---
+++ 01-color-switcher.16543fb3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");var e=null;t.addEventListener("click",(function(){e=setInterval((function(){var n="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=n,t.disabled=!0}),1e3)})),n.addEventListener("click",(function(){clearInterval(e),e=null,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.16543fb3.js.map
