40b63e9282e49e82038c5b2dc4761137fd02c17b
Andrii-Hr <andriihryb@ukr.net>
2022-10-29 18:02:50 +0300

    add 1,2 tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  let intervalId = null;
+  const PROMPT_DELAY = 1000;
+
+
+startBtn.addEventListener(`click`, ()=>{
+    intervalId = setInterval(()=>{
+    const currentColor = getRandomHexColor();
+    document.body.style.backgroundColor =  currentColor;
+    startBtn.disabled = true;
+    // stopBtn.disabled = false;
+},PROMPT_DELAY )
+});
+
+stopBtn.addEventListener(`click`,()=>{
+    clearInterval(intervalId);
+    intervalId = null;
+    startBtn.disabled = false;
+    // stopBtn.disabled = true;
+} )
